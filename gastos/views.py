63c079b3a441e364d4b4a64f4601871af5774249
Jefferson Fausto Vaz <faustovaz@gmail.com>
2023-09-29 15:03:16 -0300

    adds post method to the index view

--- gastos/views.py
+++ gastos/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 from .forms import GastoForm 
 
 views = Blueprint('views', __name__)
@@ -6,6 +6,8 @@ views = Blueprint('views', __name__)
 @views.route("/", methods=['GET', 'POST'])
 def index():
     gastoForm = GastoForm()
+    if gastoForm.validate_on_submit():
+        pass
     return render_template("index.html", form=gastoForm)
 
 
