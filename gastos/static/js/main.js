f8d28c3af047a4a63827920d9728a7c791516762
Jefferson Fausto Vaz <faustovaz@gmail.com>
2023-10-06 15:25:21 -0300

    Adds fixed buttons

--- gastos/static/js/main.js
+++ gastos/static/js/main.js
@@ -3,6 +3,11 @@
     document.addEventListener('DOMContentLoaded', function(){
         var elements = document.querySelectorAll(".sidenav");
         var instances = M.Sidenav.init(elements, {});
+
+        var btns = document.querySelectorAll('.fixed-action-btn');
+        var btnsInstances = M.FloatingActionButton.init(btns, {direction: 'left', 
+            hoverEnabled: true, 
+            toolbarEnabled: true});
     });
 
 })();
