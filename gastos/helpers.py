69da635b163eb92f85b0f9e7053e3062860bdd53
Jefferson Fausto Vaz <faustovaz@gmail.com>
2023-10-09 18:22:24 -0300

    Adds query and new way of checking monthly expenses

---
+++ gastos/helpers.py
@@ -0,0 +1,31 @@
+
+class GastosMensaisView:
+    def __init__(self, year, values):
+        self.year = year
+        self.values = values
+        self.grid = []
+        for month_number, month in months().items():
+            self.grid.append(
+                {   "month_name": month, 
+                    "key": month_number, 
+                    "total": values.get(month_number, 0),
+                    "link": f"{month_number}_{year}"
+                 })
+
+def months():
+    return {
+        1: "Janeiro",
+        2: "Fevereiro",
+        3: "Março",
+        4: "Abril",
+        5: "Maio",
+        6: "Junho",
+        7: "Julho",
+        8: "Agosto",
+        9: "Setembro",
+        10: "Outubro",
+        11: "Novembro",
+        12: "Dezembro"
+    }
+
+
