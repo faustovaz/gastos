0e90aef747efde627ee7fd21e2a7d1e963c0722c
Jefferson Fausto Vaz <faustovaz@gmail.com>
2023-10-11 12:16:09 -0300

    Adds year navigation in expenses monthly view

--- gastos/views.py
+++ gastos/views.py
@@ -1,8 +1,9 @@
 from flask import Blueprint, render_template, flash, redirect, url_for
+from functools import reduce
+from datetime import date
 from .forms import GastoForm 
 from .services import GastoService
 from .helpers import GastosMensaisView
-from functools import reduce
 
 views = Blueprint('views', __name__)
 
@@ -17,11 +18,13 @@ def add_new():
     return render_template("form.html", form=gastoForm)
 
 @views.route("/")
-@views.route("/monthly")
-def monthly():
+@views.route("/monthly/")
+@views.route("/monthly/<year>")
+def monthly(year=None):
+    year = int(year) if year and year.isnumeric() else date.today().year
     gastoService = GastoService()
-    all = gastoService.list_by_year(2023)
-    return render_template("gastos_mensais.html", gastosMensaisView=GastosMensaisView(2023, all))
+    all = gastoService.list_by_year(year)
+    return render_template("gastos_mensais.html", gastosMensaisView=GastosMensaisView(year, all))
 
 @views.route("/recurrent")
 def recurrent():
