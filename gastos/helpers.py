f3681ffa1b587c64830fd2377a1d35125dc0d7d9
Jefferson Fausto Vaz <faustovaz@gmail.com>
2023-10-21 18:16:27 -0300

    Update month expenses view

--- gastos/helpers.py
+++ gastos/helpers.py
@@ -1,36 +1,6 @@
 import functools
 from datetime import date
 
-
-class GastosMensaisView:
-    def __init__(self, year, values):
-        self.year = year
-        self.values = values
-        self.grid = []
-        for month_number, month in months().items():
-            self.grid.append(
-                {   "month_name": month, 
-                    "key": month_number, 
-                    "total": values.get(month_number, 0),
-                    "link": f"{month_number}/{year}"
-                 })
-
-class GastoMensalView:
-    def __init__(self, month, year, values, recorrentes):
-        self.year = year
-        self.month = month
-        self.values = values
-        self.recorrentes = recorrentes
-
-    def all(self):
-        return (self.values + self.recorrentes)
-
-    def total(self):
-        gastos = map(lambda g: g.quanto, self.all())
-        return functools.reduce(lambda x,y: x + y, gastos, 0)
-        
-
-
 def months():
     return {
         1: "Janeiro",
@@ -47,8 +17,45 @@ def months():
         12: "Dezembro"
     }
 
+
 def shouldInclude(date1, date2):
     first_date = date(date1.year, date1.month, 1)
     second_date = date(date2.year, date2.month, 1)
     return first_date <= second_date
+
+
+class GastosMensaisView:
+    def __init__(self, year, values):
+        self.year = year
+        self.values = values
+        self.grid = []
+        for month_number, month in months().items():
+            self.grid.append(
+                {   "month_name": month, 
+                    "key": month_number, 
+                    "total": values.get(month_number, 0),
+                    "link": f"{month_number}/{year}"
+                 })
+
+
+class GastoMensalView:
+    def __init__(self, month, year, values, recorrentes):
+        self.month = month
+        self.year = year
+        self.values = values
+        self.recorrentes = recorrentes
+        self.month_desc = months().get(int(month))
+
+    def all(self):
+        return (self.values + self.recorrentes)
+
+    def total(self):
+        gastos = map(lambda g: g.quanto, self.all())
+        return functools.reduce(lambda x,y: x + y, gastos, 0)
+
+    def link_to_month(self):
+        return f"/monthly/{self.month}/{self.year}"
+
+    def link_to_year(self):
+        return f"/monthly/{self.year}"
 
